7d84ef76da138ce0df65ded57023597f242be8b3
tammamanaswi <tammamounika58@gmail.com>
2024-07-14 18:29:21 +0530

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -1,8 +1,8 @@
 def add(x,y)
 {
-  echo 'addition of ${x} and ${y} is ${x+y}'
+  echo "addition of ${x} and ${y} is ${x+y}"
 }
 def sub(x,y)
 {
-  echo 'subtraction of ${x} and ${y} is ${x-y}'
+  echo "subtraction of ${x} and ${y} is ${x-y}"
 }
